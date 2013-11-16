b607a5776874db92bd086ca849adab35af8ddb96
An Wu <anwu@andrew.cmu.edu>
2013-11-16 14:29:09 -0500

    added user show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,8 +19,6 @@ var blocks = [];
 var blocksTable = [[], [], [], [], []];
 var panelX = 200;
 var marginX = 20;
-var maxFImgIdx = 11;
-var maxHImgIdx = 11;
 var furnitureType = 0;
 var housingType = 1;
 
@@ -55,26 +53,21 @@ function renderPanel(block, parent){
 	// feed content to the new div
 	var newImg;
 	var newShow;
-	var newLike;
 	if(block.type === housingType){
 		newImg = $('<img src=' + block.imgUrl + ' ></img>');
 		newShow = $('<a class="btn btn-mini btn-info block_button" href="/housings/' + block.id + '">Show </a>');
-		newLike = $('<a class="btn btn-mini block_button" href="/housings/' + block.id + '">Like </a>');
 	}
 	else{
 		newImg = $('<img src=' + block.imgUrl + ' ></img>');
 		newShow = $('<a class="btn btn-mini btn-info block_button" href="/furnitures/' + block.id + '">Show </a>');
-		newLike = $('<a class="btn btn-mini block_button" href="/furnitures/' + block.id + '">Like </a>');
 	}
 	var newHeader = $('<h4>'+block.title+'</h4>');
 	var newP = $('<p>' + block.description + '</p>');
 	newShow.css("left", "10px");
-	newLike.css("left", "60px");
 	newDiv.append(newImg);
 	newDiv.append(newHeader);
 	newDiv.append(newP);
 	newDiv.append(newShow);
-	newDiv.append(newLike);
 	
 	// add the panel to panels div
 	parent.append( newDiv );
